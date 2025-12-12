40cac726e7ae86ef31a44e1cbcd7b9122a49012d
STS-Engineer <administration.sts@avocarbon.com>
2025-12-12 09:47:26 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'administrationSTS',
   host: 'avo-adb-002.postgres.database.azure.com',
-  database: 'RFQ_DATA',
+  database: 'Costing_DB',
   password: 'St$@0987',
   port: 5432,
   ssl: {
